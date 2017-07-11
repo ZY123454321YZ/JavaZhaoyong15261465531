c3a1fb73257513525d8450ce07b80ca4eaccb522
zhaoyong <834162364@qq.com>
2017-07-11 08:03:18 +0800

    2017/07/11

---
+++ Reflect/src/com/action/Reflect/ReflectTest.java
@@ -0,0 +1,42 @@
+package com.action.Reflect;
+
+import java.lang.reflect.*;
+
+import javax.activation.FileDataSource;
+
+public class ReflectTest {
+	private String fname;
+	private String lname;
+	public ReflectTest(String fname, String lname) {
+		this.fname = fname;
+		this.lname = lname;
+	}
+	public static void main(String[] args) {
+		try {
+			ReflectTest f=new ReflectTest("java","aspect");
+			run(f);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private static void run(Object o) throws Exception {
+            	Field[]	fields=o.getClass().getDeclaredFields();
+		        System.out.println("替换之前的:");
+		        for(Field file:fields){
+		        	System.out.println(file.getName()+"="+file.get(o));
+		        	if(file.getType().equals(java.lang.String.class)){
+		        		file.setAccessible(true);
+		        		String obj=
+		        				(String) file.get(o);
+                       file.set(o,obj.replace("a","b"));
+		        	}
+		        }
+		System.out.println("替换之后的:");
+		for(Field file:fields){
+        	System.out.println(file.getName()+"="+file.get(o));
+        }
+	}
+	
+	
+
+}
---
+++ Servlet/src/com/huawei/FirstServlet.java
@@ -0,0 +1,47 @@
+package com.huawei;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    /**
+     * Default constructor. 
+     */
+    public FirstServlet() {
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JDBCUtil util=new JDBCUtil();
+		try {  
+			while(true){
+				util.insertPuser("fuck","156");
+		}
+				
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
